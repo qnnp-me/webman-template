bc0896688ab7671afdc23c475098bd4926201ed7
qnnp <wjp@pfan.ltd>
2024-05-23 00:11:25 +0800

    Init
    Signed-off-by: qnnp <wjp@pfan.ltd>

--- tools/create-project.php
+++ tools/create-project.php
@@ -6,5 +6,10 @@ if (file_exists($project_name)) {
   echo "$project_name already exists";
   exit;
 }
+$git_exists = shell_exec("which git");
+if (!$git_exists) {
+  echo "git not found";
+  exit;
+}
 shell_exec("git clone https://github.com/qnnp-me/webman-template.git $project_name");
 `rm -rf $project_name/.git`;
