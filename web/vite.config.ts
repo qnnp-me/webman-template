154e21adc7b53d3ddb0172db258ec69989a174b9
qnnp <wjp@pfan.ltd>
2024-05-23 01:38:42 +0800

    Init
    Signed-off-by: qnnp <wjp@pfan.ltd>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dynamicImport from "vite-plugin-dynamic-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dynamicImport()],
+  build: {
+    outDir: '../server/public',
+    emptyOutDir: true,
+  },
 })
