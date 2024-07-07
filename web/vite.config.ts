ec84580d7e80c4282742cfc2b173adc237d34705
qnnp <wjp@pfan.ltd>
2024-07-07 20:16:15 +0800

    feat(vite-config): 优化块拆分并添加可视化工具插件
    -为chunkSplitPlugin设置unbundle策略。-为chunkSplitPlugin启用useEntryName选项。
    -集成rollup-plugin-visualizer以进行构建分析。-在rollupOptions中重构manualChunks逻辑。
    -将别名 “@ root” 添加到vite配置。
    
    此更改更新了vite.config.ts以改进块拆分行为和
    添加用于生成分析的可视化工具插件。它还清理和重构
    manualChunks逻辑和扩展解析别名。
    
    Signed-off-by: qnnp <wjp@pfan.ltd>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,22 +1,62 @@
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
-import dynamicImport from 'vite-plugin-dynamic-import';
-
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
-
-import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    chunkSplitPlugin(),
+    chunkSplitPlugin({
+      strategy: 'unbundle',
+      useEntryName: true,
+    }),
+    visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      emitFile: false,
+      filename: 'test.html', //分析图生成的文件名
+      open: true, //如果存在本地服务端口，将在打包后自动展示
+    }),
   ],
   build: {
     emptyOutDir: true,
     outDir: '../server/public',
     rollupOptions: {
-      output: {},
+      output: {
+        manualChunks: (id, meta) => {
+          // if (id.includes('home/pages')) {
+          //   const key = id.replace(/.*home\/pages\/([^/]+).*/, '$1');
+          //   return `home-pages-${key}`;
+          // }
+          // if (id.includes('antd/es/result/')) {
+          //   const info = meta.getModuleInfo(id);
+          //   console.log(info);
+          //   console.log(info?.isIncluded);
+          // }
+          // if (id.includes('dayjs')){
+          //   return 'dayjs';
+          // }
+          // if (id.includes('lodash')){
+          //   return 'lodash';
+          // }
+          // if (/@ant-design\/icons-svg\/es\/asn\/[A-Z][a-zA-Z0-9]+/.test(id)) {
+          //   return 'antd-icon';
+          //   // const key = id.replace(/.*@ant-design\/icons-svg\/es\/asn\/([A-Z][a-zA-Z0-9]+).*/, '$1');
+          //   // return `antd-icon-${key}`;
+          // }
+          // if (/@ant-design\/[^/]+/.test(id)){
+          //   return 'ant-design';
+          //   // const key = id.replace(/.*@ant-design\/([^/]+).*/, '$1');
+          //   // return `antd-pro-${key}`;
+          // }
+          // console.log(id);
+          // if (id.includes('node_modules')) {
+          //   return 'vendor';
+          // }
+        },
+      },
     },
   },
   server: {
@@ -35,6 +75,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      '@root': resolve(__dirname),
       '@common': resolve(__dirname, 'src'),
       '@admin': resolve(__dirname, 'admin'),
       '@home': resolve(__dirname, 'home'),
