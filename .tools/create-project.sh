658d319270d2dd92e9c7a2b7f3355adc63dac171
qnnp <wjp@pfan.ltd>
2024-07-18 12:57:50 +0800

    feat(tools): 添加创建项目脚本，改进构建脚本组织
    添加了一个新的创建项目脚本.create-project.sh，该脚本从模板克隆并安装依赖项，便于快速启动新项目。同时，将构建相关脚本移动到.tools目录下，优化了项目结构。
    
    BREAKING CHANGE: 构建和创建项目流程现在依赖于.tools目录下的脚本。请确保这个目录存在于您的项目中，以避免任何中断。
    
    Signed-off-by: qnnp <wjp@pfan.ltd>

---
+++ .tools/create-project.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+WORKDIR=$(pwd)
+BASEDIR=$(dirname "$0")
+
+read -r -p "Enter project name: " project_name
+if [ -z "$project_name" ]; then
+  project_name="webman"
+fi
+
+if [ -d "$project_name" ]; then
+  echo "$project_name already exists"
+  exit
+fi
+
+# clone template
+git_exists=$(which git)
+
+if [ -z "$git_exists" ]; then
+  echo "git not found"
+  exit
+fi
+
+git clone https://github.com/qnnp-me/webman-template.git "$project_name"
+rm -rf "$project_name/.git"
+rm -rf "$project_name/.tools/create-project.sh"
+
+# install dependencies
+
+composer_exists=$(which composer)
+
+if [ -z "$composer_exists" ]; then
+  echo "composer not found"
+  exit
+fi
+cd "$BASEDIR/$project_name/server" || exit
+composer install
+
+cd "$BASEDIR/$project_name/web" || exit
+
+pnpm_exists=$(which pnpm)
+
+if [ -z "$pnpm_exists" ]; then
+  echo "pnpm not found"
+  exit
+fi
+pnpm i
+
+cd "$WORKDIR" || exit
