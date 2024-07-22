9dcc9b8477d81cdb035192c3d72da59f17aecdce
qnnp <wjp@pfan.ltd>
2024-07-22 18:09:54 +0800

    refactor(server-config): 更新Webman插件的console应用配置
    调整了console应用程序的配置文件，主要对缩进和换行符进行了规范化，以提高代码的可读性和一致性。此外，增加了`custom_ini`配置项的内存限制设置，从256M增加到512M，以满足更高内存需求的场景。
    
    BREAKING CHANGE: 配置文件中的缩进和换行符已规范化。如果您的自动化工具或部署流程依赖于以前的格式，请更新以适应新的格式规范。
    
    Signed-off-by: qnnp <wjp@pfan.ltd>

--- server/config/plugin/webman/console/app.php
+++ server/config/plugin/webman/console/app.php
@@ -1,24 +1,24 @@
 <?php
 return [
-    'enable' => true,
+  'enable' => true,
 
-    'build_dir'  => BASE_PATH . DIRECTORY_SEPARATOR . 'build',
+  'build_dir'  => BASE_PATH . DIRECTORY_SEPARATOR . 'build',
 
-    'phar_filename' => 'server.phar',
+  'phar_filename' => 'server.phar',
 
-    'bin_filename' => 'server.bin',
+  'bin_filename' => 'server.bin',
 
-    'signature_algorithm'=> Phar::SHA256, //set the signature algorithm for a phar and apply it. The signature algorithm must be one of Phar::MD5, Phar::SHA1, Phar::SHA256, Phar::SHA512, or Phar::OPENSSL.
+  'signature_algorithm' => Phar::SHA256,   //set the signature algorithm for a phar and apply it. The signature algorithm must be one of Phar::MD5, Phar::SHA1, Phar::SHA256, Phar::SHA512, or Phar::OPENSSL.
 
-    'private_key_file'  => '', // The file path for certificate or OpenSSL private key file.
+  'private_key_file'  => '', // The file path for certificate or OpenSSL private key file.
 
-    'exclude_pattern'   => '#^(?!.*(composer.json|/.github/|/.idea/|/.git/|/.setting/|/runtime/|/vendor-bin/|/build/|/vendor/webman/admin/|/plugin/admin/public/upload/))(.*)$#',
+  'exclude_pattern'   => '#^(?!.*(composer.json|/.github/|/.idea/|/.git/|/.setting/|/runtime/|/vendor-bin/|/build/|/vendor/webman/admin/|/plugin/admin/public/upload/))(.*)$#',
 
-    'exclude_files'     => [
-        '.env', 'LICENSE', 'composer.json', 'composer.lock', 'start.php', 'webman.phar', 'webman.bin', 'server.phar', 'server.bin', '.env.example', '.gitignore', '.gitkeep'
-    ],
+  'exclude_files'     => [
+    '.env', 'LICENSE', 'composer.json', 'composer.lock', 'start.php', 'webman.phar', 'webman.bin', 'server.phar', 'server.bin', '.gitignore', '.gitkeep'
+  ],
 
-    'custom_ini' => '
-memory_limit = 256M
-    ',
+  'custom_ini' => "
+memory_limit = 512M
+    ",
 ];
