1f618d077a47baa6cc0d24e818a55b6d647d74eb
qnnp <wjp@pfan.ltd>
2024-10-02 16:35:00 +0800

    改进服务器初始化文件处理
    - 新增权重属性，用于排序初始化文件执行顺序
    - 优化初始化文件的检测和执行逻辑，支持非静态方法
    - 增加错误处理，捕获并输出初始化文件执行中的异常信息
    -调整初始化文件的路径，从'support/init'改为'app/init'
    - 对初始化文件进行排序，按权重属性优先执行
    - 添加初始化文件执行结果和错误信息的输出

--- server/app/init/ReleaseFiles.php
+++ server/app/init/ReleaseFiles.php
@@ -6,6 +6,7 @@ use Phar;
 
 class ReleaseFiles
 {
+  public int $weight = 0;
 
   function run(): void
   {
--- server/process/Init.php
+++ server/process/Init.php
@@ -2,26 +2,55 @@
 
 namespace process;
 
+use Throwable;
+
 class Init
 {
   public function __construct()
   {
-    $base_path = base_path('support/init');
+    $base_path = base_path('app/init');
     $all_files = getAllFiles($base_path);
+    $init_functions = [];
+    echo str_pad(' Processing Init Files ', 60, '=', STR_PAD_BOTH) . PHP_EOL;
     foreach ($all_files as $file) {
+      $relative_path = str_replace(base_path(), '', $file);
       $class = str_replace(base_path(), '', str_replace(".php", '', $file));
       $class = str_replace('/', '\\', $class);
       if (class_exists($class) && method_exists($class, 'run')) {
-        $ref = new \ReflectionClass($class);
-        if ($ref->hasMethod('run')) {
-          $method = $ref->getMethod('run');
-          if ($method->isStatic()) {
-            $class::run();
-          }else{
-            $ref->newInstance()->run();
+        $class_ref = new \ReflectionClass($class);
+        if ($class_ref->hasMethod('run')) {
+          try {
+            $weight = $class_ref->hasProperty('weight') && ($weight_ref = $class_ref->getProperty('weight'))->isPublic()
+              ? (int)($weight_ref->isStatic() ? $class_ref->newInstance()::$weight : $class_ref->newInstance()->weight)
+              : 10;
+          } catch (Throwable $e) {
+            $weight = 10;
           }
+          $method = $class_ref->getMethod('run');
+          $function = [
+            'weight' => $weight,
+            'file' => $relative_path,
+          ];
+          if ($method->isStatic()) {
+            $function['call'] = [$class, 'run'];
+          } else {
+            $function['call'] = [new $class, 'run'];
+          }
+          $init_functions[] = $function;
         }
       }
     }
+    usort($init_functions, function ($a, $b) {
+      return $a['weight'] <=> $b['weight'];
+    });
+    foreach ($init_functions as $function) {
+      try {
+        echo str_pad(" Init File: {$function['file']} -> {$function['call'][1]} ", 60, '-', STR_PAD_BOTH) . PHP_EOL;
+        call_user_func($function['call']);
+      } catch (\Throwable $th) {
+        echo "Init File Error: {$th->getMessage()}\n{$th->getTraceAsString()}\n";
+      }
+    }
+    echo str_pad(' Init Finished ', 60, '=', STR_PAD_BOTH) . PHP_EOL;
   }
 }
