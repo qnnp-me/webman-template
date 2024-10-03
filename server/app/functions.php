1685d1ca8f6e505392913ec96fde78069ebc06a8
qnnp <wjp@pfan.ltd>
2024-10-03 18:28:29 +0800

    新增文件遍历函数get_files和更新.gitignore
    - 在server/app/functions.php中添加了get_files函数，用于递归遍历指定路径下的所有文件，并根据包含和排除规则筛选文件。
    - 更新了项目根目录下的.gitignore文件，添加了针对Linux、Windows和macOS系统的临时文件和特定目录的忽略规则，以优化Git跟踪文件的管理。

--- server/app/functions.php
+++ server/app/functions.php
@@ -58,6 +58,52 @@ function getAllFiles(string $abs_path): array
   return $list;
 }
 
+/**
+ * @param string $path file or dir path
+ * @param array|string $include Ex: `'*.php'` `['.php', '.js']` `['~^[A-Z].*\.php$~', '/\.js$/']`
+ * @param array|string $exclude Ex: `'*.php'` `['.php', '.js']` `['~^[A-Z].*\.php$~', '/\.js$/']`
+ * @return Generator
+ */
+function get_files(string $path, array|string $include = [], array|string $exclude = []): Generator
+{
+  if (!is_array($include)) $include = [$include];
+  if (!is_array($exclude)) $exclude = [$exclude];
+  if (is_file($path)) {
+    yield $path;
+  } else {
+    $items = is_dir($path) ? scandir($path) : [];
+    foreach ($items as $item) {
+      if (in_array($item, ['.', '..'])) continue;
+      $item_path = $path . DIRECTORY_SEPARATOR . $item;
+      if (is_dir($item_path)) {
+        yield from get_files($item_path, $include, $exclude);
+      } else {
+        $match_check = function (array $include, $item, bool $default = false) {
+          foreach ($include as $i) {
+            $is_preg = preg_match('~^([/#\~]).+([/#\~])$~', $i);
+            if ($is_preg && preg_match($i, $item)) {
+              return true;
+            }
+            $is_pan = !$is_preg && (str_starts_with($i, '*') || str_starts_with($i, '.'));
+            if ($is_pan && str_ends_with($item, str_replace('*', '', $i))) {
+              return true;
+            }
+            if ($i == $item) {
+              return true;
+            }
+          }
+          return $default;
+        };
+        $include_match = $match_check($include, $item, empty($include));
+        $exclude_match = $match_check($exclude, $item, false);
+        if ($include_match && $exclude_match) continue;
+        if (!$include_match && !$exclude_match) continue;
+        yield $item_path;
+      }
+    }
+  }
+}
+
 function ss(): SessionModule
 {
   return SessionModule::getInstance();
